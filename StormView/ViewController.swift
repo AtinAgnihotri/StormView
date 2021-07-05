be61619701891103d24cda4f143f020feaeecaae
AtinAgnihotri <atinagnihotri@gmail.com>
2021-07-05 21:56:04 +0530

    Adds basic layout to ViewController

--- StormView/ViewController.swift
+++ StormView/ViewController.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     var pictures = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadImagesFromBundle()
+        print(pictures)
+    }
+    
+    func loadImagesFromBundle() {
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -21,10 +26,17 @@ class ViewController: UIViewController {
                 pictures.append(item)
             }
         }
-        
-        print(pictures)
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 
 }
 
