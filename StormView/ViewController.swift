aeb8e7faddff4f48cd776ce3f63d88cb5cecfcd2
AtinAgnihotri <atinagnihotri@gmail.com>
2021-07-23 10:54:34 +0530

    Updates layout to CollectionView

--- StormView/ViewController.swift
+++ StormView/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UICollectionViewController {
 
     var pictures = [String]()
     
@@ -44,18 +44,26 @@ class ViewController: UITableViewController {
         }
         pictures.sort()
         DispatchQueue.main.async { [weak self] in
-            self?.tableView.reloadData()
+            self?.collectionView.reloadData()
         }
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return pictures.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
-        return cell
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+//        cell.textLabel?.text = pictures[indexPath.row]
+//        return cell
+//    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell
     }
     
     func giveTitleTextForDetailVC(_ index: Int) -> String {
